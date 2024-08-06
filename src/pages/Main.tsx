bd62a0db613640516ca5ebff23ea52308776a322
Zero1016 <iolpd42@gmail.com>
2024-08-06 12:58:25 +0900

    feat: rtk를 추가합니다.

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,14 +1,27 @@
 import styled from '@emotion/styled';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { OneTodo } from '../components';
+import type { CounterState } from '../store/counterSlice.ts';
+import { decrement } from '../store/counterSlice.ts';
+import { increment } from '../store/counterSlice.ts';
 
 function Main() {
+  const counter = useSelector((state: CounterState) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <>
       <S.Container>
-        <OneTodo />
-        <OneTodo />
-        <OneTodo />
+        Counter = {counter}
+        <button
+          aria-label='Increment value'
+          onClick={() => dispatch(increment())}>
+          increment
+        </button>
+        <button
+          aria-label='Decrement  value'
+          onClick={() => dispatch(decrement())}>
+          decrement
+        </button>
       </S.Container>
     </>
   );
--- src/shared/layout.tsx
+++ src/shared/layout.tsx
@@ -1,14 +1,16 @@
 import { Global } from '@emotion/react';
+import { Provider } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 
+import { store } from '../store';
 import { globalStyles } from '../styles/globalStyles.ts';
 
 function Layout() {
   return (
-    <>
+    <Provider store={store}>
       <Global styles={globalStyles} />
       <Outlet />
-    </>
+    </Provider>
   );
 }
 
